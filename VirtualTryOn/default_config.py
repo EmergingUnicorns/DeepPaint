5001047f9278a3ce0b8d605b06501cb6ed35ccc0
anmol <anmolduainter@gmail.com>
2023-10-17 18:45:23 +0800

    added config

--- VirtualTryOn/default_config.py
+++ VirtualTryOn/default_config.py
@@ -16,5 +16,18 @@ def get_config_default():
     d.num_class_images = 100
     d.output_dir = "text-inversion-model"
     d.seed = None
+    d.resolution = 512
+    d.center_crop = False
+    d.train_text_encoder = True
+    d.train_batch_size = 4
+    d.sample_batch_size = 4
+    d.num_train_epochs = 1
+    d.max_train_steps = None
+    d.gradient_accumulation_steps = 1
+    d.gradient_checkpointing = True
+    d.learning_rate = 5e-6
+    d.scale_lr = False
+    d.lr_scheduler = "constant"
+    d.lr_warmup_steps = 500
     
     return d

9150abb788acb1a52cad52ce7851b6fa09bdac53
anmol <anmolduainter@gmail.com>
2023-10-18 13:06:20 +0800

    updated config

--- VirtualTryOn/default_config.py
+++ VirtualTryOn/default_config.py
@@ -20,30 +20,31 @@ class DotDict(dict):
     resolution: The resolution for input images, all the images in the train/validation dataset will be resized to this.
     center_crop: Whether to center crop the input images to the resolution. If not set, the images will be randomly cropped. The images will be resized to the resolution first before cropping.
     train_text_encoder: Whether to train the text encoder.
-    train_batch_size:
-    sample_batch_size:
+    train_batch_size:Batch size (per device) for the training dataloader.
+    sample_batch_size:Batch size (per device) for sampling images.
     num_train_epochs:
-    max_train_steps:
-    gradient_accumulation_steps:
-    gradient_checkpointing:
-    learning_rate:
-    scale_lr:
-    lr_scheduler:
-    lr_warmup_steps:
-    use_8bit_adam:
-    adam_beta1:
-    adam_beta2:
-    adam_weight_decay:
-    adam_epsilon:
-    max_grad_norm:
-    push_to_hub:
-    hub_token:
-    hub_model_id:
+    max_train_steps:Total number of training steps to perform.  If provided, overrides num_train_epochs.
+    gradient_accumulation_steps:Number of updates steps to accumulate before performing a backward/update pass.
+    gradient_checkpointing:Whether or not to use gradient checkpointing to save memory at the expense of slower backward pass.
+    learning_rate:Initial learning rate
+    scale_lr:Scale the learning rate by the number of GPUs, gradient accumulation steps, and batch size.
+    lr_scheduler:The scheduler type to use.
+    lr_warmup_steps:Number of steps for the warmup in the lr scheduler.
+    use_8bit_adam:Whether or not to use 8-bit Adam from bitsandbytes.
+    adam_beta1:The beta1 parameter for the Adam optimizer.
+    adam_beta2:The beta2 parameter for the Adam optimizer.
+    adam_weight_decay:Weight decay to use.
+    adam_epsilon:Epsilon value for the Adam optimizer
+    max_grad_norm:Max gradient norm.
+    push_to_hub:Whether or not to push the model to the Hub.
+    hub_token:The token to use to push to the Model Hub.
+    hub_model_id:The name of the repository to keep in sync with the local `output_dir`.
     logging_dir:
-    mixed_precision:
-    checkpointing_steps:
-    checkpoints_total_limit:
-    resume_from_checkpoint:
+    mixed_precision:Whether to use mixed precision.
+    local_rank: For distributed training: local_rank
+    checkpointing_steps:Save a checkpoint of the training state every X updates.
+    checkpoints_total_limit:Max number of checkpoints to store.
+    resume_from_checkpoint:Whether training should be resumed from a previous checkpoint. Use a path saved by' `--checkpointing_steps`, or `"latest"` to automatically select the last available checkpoint.
     
 '''
 def get_config_default():
@@ -87,7 +88,4 @@ def get_config_default():
     d.checkpointing_steps = 500
     d.checkpoints_total_limit = None
     d.resume_from_checkpoint = None
-
-
-    
     return d
